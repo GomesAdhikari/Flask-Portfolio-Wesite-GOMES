19c520b88246ddce17697eaa7a0632032931db9b
GomesAdhikari <gomesadhikari@gmail.com>
2024-11-09 08:23:39 +0530

    added links to app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ PROJECTS = [
     {
         "title":'Heart Disease Risk Prediction With AI health Advice',
         "About":'In this project, I developed a Heart Disease Risk Prediction system using advanced AI techniques. By analyzing key health indicators such as age, blood pressure, cholesterol, and more, the system predicts the likelihood of a patient developing heart disease. I utilized Regression models with the help of Scikit-learn, Pandas for data preprocessing, and GenAI to provide personalized health advice. The model not only offers a prediction but also provides actionable health recommendations tailored to the individuals risk factors, empowering users to take proactive steps toward improving their heart health.',
-        "Link":'link'
+        "Link":'https://heart-disease-risk-prediction-with-ai.onrender.com/'
     },
     {
         "title":'New Delhi LIVE data House Price prediction with property Recommendation',
@@ -16,12 +16,12 @@ PROJECTS = [
     {
         "title":'AI powered SQL query Chatbot',
         "About":"In this project, I developed an AI-powered SQL Query Chatbot that allows users to interact with their MySQL or SQL Server databases through a conversational interface. By inputting their database credentials, users can simply ask questions about their data in natural language, and the chatbot automatically generates and executes the corresponding SQL queries. This chatbot streamlines the process of querying databases, making it easier for both technical and non-technical users to retrieve insights without needing to write SQL code manually.",
-        "Link":'link'
+        "Link":'https://youtu.be/SnaRPqkUo7Y?si=dEhSjmjSNYiPohUb'
     },
     {
         "title":'Skin Cancer Detection',
         "About":"The skin cancer detection project aims to classify skin lesions as either malignant or benign based on images of the skin. Using deep learning techniques, particularly convolutional neural networks (CNNs), the model is trained on a large dataset of labeled images of skin lesions. The primary goal is to assist healthcare professionals by providing an automated system for detecting cancerous growths early, leading to timely intervention. The project involves preprocessing the images, applying data augmentation to increase model robustness, and using classification algorithms to distinguish between malignant (cancerous) and benign (non-cancerous) lesions. Accuracy, sensitivity, and specificity are key metrics used to evaluate the model’s performance in real-world settings.",
-        "Link":'link'
+        "Link":'https://skin-cancer-detection-flask-app-with-gjkn.onrender.com/'
     }
 
 ]
